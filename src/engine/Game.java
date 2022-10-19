b24018c080baaa1d3a6d22f79006fa676aa6fe50
ErikTellier <erik.tellier.etps@gmail.com>
2022-10-19 09:51:05 +0200

    added delta loop

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -10,10 +10,10 @@ public class Game implements Runnable{
 
     private boolean running = false;
     private Thread thread;
-
-
     private BufferStrategy bs;
     private Graphics g;
+
+
     public Game(String title, int width, int height){
         this.title = title;
         this.width = width;
@@ -25,7 +25,8 @@ public class Game implements Runnable{
     private void init(){
         window = new Window(title, width, height);
     }
-    private void update(){}
+    private void update(){
+    }
     private void render(){
         bs = window.getCanvas().getBufferStrategy();
         if(bs == null){
@@ -37,6 +38,8 @@ public class Game implements Runnable{
         g.clearRect(0, 0, width, height);
         //Draw Here!
 
+        g.setColor(Color.red);
+        g.fillRect(50, 50, 50, 70);
 
         //End Drawing!
         bs.show();
@@ -47,8 +50,31 @@ public class Game implements Runnable{
     public void run() {
         init();
 
+        //Get the system time
+        long lastTime = System.nanoTime();
+        //Specify how many seconds there are in a minute as a double
+        //store as a double cause 60 sec in nanosec is big and store as final so it can't be changed
+        final double ticks = 60D;
+        //Set definition of how many ticks per 1000000000 ns or 1 sec
+        double ns = 1000000000 / ticks;
+        double delta = 0;
+
         while(running){
-            update();
+            //Update the time
+            long now = System.nanoTime();
+            //calculate change in time since last known time
+            delta += (now - lastTime) / ns;
+            //update last known time
+            lastTime = now;
+            //continue while delta is less than or equal to 1
+            if(delta >= 1){
+                //Go through one update
+                update();
+
+                delta--;
+            }
+
+            //Render outside of delta
             render();
         }
 
